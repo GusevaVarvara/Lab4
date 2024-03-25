b927f8b3cc0eb7e22a97a052773e99a4623e1eb8
GusevaVarvara <st119025@student.spbu.ru>
2024-03-25 18:45:43 +0300

    Добавлено автоматическое определение размера изображения на основе количества вершин.

--- main.cpp
+++ main.cpp
@@ -2,14 +2,17 @@
 #include "graph_reader.h"
 #include "graph_visualization.h"
 #include "graph_drawing.h"
+#include <cmath>
 
 
 int main() {
     int V, E;
     std::vector<std::vector<int>> adjacencyMatrix = readGraph("input.txt", V, E);
 
-    const int width = 2000; 
-    const int height = 2000; 
+    int sideLength = static_cast<int>(std::round(std::sqrt(V)));
+
+    const int width = sideLength * 88;
+    const int height = sideLength * 88;
     std::vector<std::vector<uint8_t>> image(height, std::vector<uint8_t>(width, 255)); 
 
     std::vector<Point> points(V);
